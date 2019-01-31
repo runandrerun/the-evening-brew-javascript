2bca5ebac8080b3b66d73f0656c146b5583b7065
runandrerun <the.asantiagojr@gmail.com>
2019-01-30 22:11:57 -0500

    Finished

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
         </td>
       </tr>
     </table>
+    <hr/>
     `
   };
 
@@ -47,11 +48,167 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.getElementById('story-content');
     const previewContent = document.getElementById('preview-content');
     // previewContent.innerHTML = body.value;
-    previewContent.innerHTML = tinyMCE.get('story-content').getContent()
+    previewContent.innerHTML = tinyMCE.get('story-content').getContent();
 
     const title = document.getElementById('story-title');
     const previewTitle = document.getElementById('preview-title');
     previewTitle.innerHTML = title.value;
   };
+
+  const submitButton = document.getElementById('submit-story');
+
+  submitButton.onclick = (e) => {
+    e.preventDefault();
+    const storyContent = tinyMCE.get('story-content').getContent();
+    const storyTitle = document.getElementById('story-title').value;
+
+    const postData = {
+      title: storyTitle,
+      body: storyContent,
+    }
+    fetch('http://localhost:3000/posts', {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+    .then(res => res.json())
+    .then(story => {
+      document.getElementById('story-title').value = '';
+      tinyMCE.activeEditor.setContent('');
+
+      document.getElementById('story-index').innerHTML = '';
+      buildStories();
+    });
+  };
+
+  const buildStories = () => {
+
+      const storyIndices = document.getElementById('story-index');
+      const storiesUrl = 'http://localhost:3000/posts';
+
+      return fetch(storiesUrl)
+        .then(res => res.json())
+        .then(stories => {
+          if (stories) {
+            const storiesList = document.createElement('ul');
+            const listName = document.createElement('h4');
+            listName.innerText = "Stories";
+            const hr = document.createElement('hr');
+            document.getElementById('story-index').append(hr, listName, storiesList);
+            stories.forEach(story => {
+              const storyItem = document.createElement('li');
+              storyItem.id = "story-" + story['title'];
+              storyItem.innerText = story['title'];
+              storyItem.onclick = (e) => {
+                e.preventDefault();
+                const storyData = {
+                  id: story['id'],
+                  title: story['title'],
+                  body: story['body'],
+                };
+                renderStory(storyData);
+              };
+
+              storiesList.append(storyItem);
+            })
+          } else {
+            return 'No Stories!';
+          };
+        });
+  };
+
+  const renderStory = (story) => {
+
+    console.log(story);
+    const storyTitle = story['title'];
+    let activeEditor = tinyMCE.get('story-content');
+    let content = story['body'];
+    if(activeEditor!==null){
+      document.getElementById('story-title').value = storyTitle;
+      activeEditor.setContent(content);
+
+      document.getElementById('preview-title').innerHTML = storyTitle;
+      document.getElementById('preview-content').innerHTML = content;
+      document.getElementById('preview-title').dataset.id = story['id'];
+      document.getElementById('new-story-btn').innerHTML = '';
+      const newStoryButton = document.createElement('btn');
+      newStoryButton.className = 'btn btn-primary';
+      newStoryButton.innerText = 'New Story';
+      document.getElementById('new-story-btn').append(newStoryButton);
+
+      newStoryButton.onclick = (e) => {
+        e.preventDefault();
+        document.getElementById('new-story-btn').innerHTML = '';
+        document.getElementById('preview-title').innerHTML = '';
+        document.getElementById('preview-content').innerHTML = '';
+
+        let activeEditor = tinyMCE.get('story-content');
+        let content = '';
+        if(activeEditor !== null){
+          document.getElementById('story-title').value = '';
+          activeEditor.setContent(content);
+          document.getElementById('preview-btn').innerText = 'Preview';
+          document.getElementById('preview-btn').onclick = (e) => {
+            e.preventDefault();
+
+            const body = document.getElementById('story-content');
+            const previewContent = document.getElementById('preview-content');
+            // previewContent.innerHTML = body.value;
+            previewContent.innerHTML = tinyMCE.get('story-content').getContent()
+
+            const title = document.getElementById('story-title');
+            const previewTitle = document.getElementById('preview-title');
+            previewTitle.innerHTML = title.value;
+            previewTitle.dataset.id = story['id'];
+          }
+        }
+      };
+
+      const previewButton = document.getElementById('preview-btn');
+      previewButton.innerText = 'Update';
+      previewButton.onclick = (e) => {
+        e.preventDefault();
+        const storyId = parseInt(document.getElementById('preview-title').dataset.id);
+        const storyTitle = document.getElementById('story-title').value;
+        const storyBody = tinyMCE.get('story-content').getContent();
+        const storyData = {
+          id: storyId,
+          title: storyTitle,
+          body: storyBody,
+        };
+        updateStory(storyData);
+
+        document.getElementById('story-index').innerHTML = '';
+        // buildStories();
+      }
+
+    };
+  };
+
+  const updateStory = (storyData) => {
+    // console.log('Update', storyData);
+    const patchData = {
+      title: document.getElementById('story-title').value,
+      body: tinyMCE.get('story-content').getContent(),
+    };
+    const url = `http://localhost:3000/posts/${storyData['id']}`
+    fetch(url, {
+      method: 'PATCH',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(patchData),
+    })
+    .then(res => res.json())
+    .then(res => {
+      buildStories();
+    }, () => builderStories());
+  };
+
+  buildStories();
 
 });
